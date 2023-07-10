ed19d725a374675e56accfc820bd7105d73feed8
ronishg27 <iamrg2060@gmail.com>
2023-07-10 18:20:17 +0545

    text form completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import Navbar from "./components/Navbar";
-import TextForm from "./components/TextForm";
+// import TextForm from "./components/TextForm";
+import About from "./components/about";
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils" aboutText="About TextUtils" />
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze."/>
-        
+        {/* <TextForm heading="Enter the text to analyze."/> */}
+        <About></About>
       </div>
     </>
   );
