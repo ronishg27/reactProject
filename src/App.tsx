16cf693c2bc71e1826bc6bba236807d6dd9355ee
ronishg27 <iamrg2060@gmail.com>
2023-07-11 09:46:49 +0545

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-// import About from "./components/about";
+// import About from "./components/About";
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils" aboutText="About TextUtils" />
       <div className="container my-3">
+        {/* <nav>
+          <ul>
+            <li>
+              <link to='./components/TextForm'>TextForm</link></li>
+            <li><link to='./components/About"'>About</link></li>
+          </ul>
+        </nav> */}
         <TextForm heading="Enter the text to analyze." />
         {/* <About /> */}
       </div>
+
     </>
   );
 }
